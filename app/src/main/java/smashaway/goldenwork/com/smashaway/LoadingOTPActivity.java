8bcc1360847585e8ab21a8805374aaf315d178da
kaderem <kaderem@gmail.com>
2017-08-12 00:46:36 +0200

    First commit

---
+++ app/src/main/java/smashaway/goldenwork/com/smashaway/LoadingOTPActivity.java
@@ -0,0 +1,30 @@
+package smashaway.goldenwork.com.smashaway;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LoadingOTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_otp);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoCN();
+            }
+        });
+    }
+    private void gotoCN() {
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+    }
+}
